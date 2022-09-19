b0aa9b449fca6740c0d23705d139d353ae1b7986
Francisco Solis <fran@theprogramsrc.xyz>
2022-09-19 16:48:50 -0300

    Fixed login notification email

--- resources/views/mail/account/login.blade.php
+++ resources/views/mail/account/login.blade.php
@@ -4,7 +4,9 @@
 We've detected a new login in your account. If you didn't make this login, please reset your password immediately.
 
 @component('mail::panel')
-IP Address: {{ $ip }}. Location: {{ $location }}. Device: {{ $device }}.
+IP Address: <b>{{ $ip }}</b>. <br/>
+Location: <b>{{ $location }}</b>. <br/>
+Device: <b>{{ $device }}</b>.
 @endcomponent
 
 @component('mail::button', ['url' => route('account')])
